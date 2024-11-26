9e0c22a786bf2ce029d872cbdedffeb10f87ff71
Oussama Aouragh <oussama1800@hotmail.fr>
2024-11-26 12:30:59 +0100

    Gangstaname

---
+++ SlnLes01VisualStudioEnWpf/ConsoleGangsterName/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleGangsterName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine(@"************************
+| GANGSTA NAME BUILDER |
+************************");
+            Console.Write("Give the first name of any Disney character: ");
+            string naam = Console.ReadLine();
+            Console.Write("Give any workbench tool: ");
+            string tool = Console.ReadLine();
+            Console.Write("What is your last name: ");
+            string lastname = Console.ReadLine();
+
+            Console.Write($"Your gangsta name: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{naam} 'the {tool}' {lastname}");
+        }   
+    }
+}
