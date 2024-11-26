fdcee4750e467050e1b2524ceacfb2eb821037f8
Oussama Aouragh <oussama1800@hotmail.fr>
2024-11-26 12:00:39 +0100

    fcfr

---
+++ SlnLes01VisualStudioEnWpf/ConsoleHelloWereld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleHelloWereld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hallo, Wereld! ");
+            Console.Write("Hoe heet je: ");
+            string naam = Console.ReadLine();
+            Console.WriteLine(@$"Aangename kennismaking, {naam}
+druk een toets om verder te gaan...");
+        }   
+    }
+}
